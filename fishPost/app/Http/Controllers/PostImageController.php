702ccd56dd7a9b65debe33daeeb5802e7702e4e0
kinoshitaken123 <k1k7s9e3@gmail.com>
2022-11-12 10:43:31 +0900

    feature フォーム投稿機能作成

--- fishPost/app/Http/Controllers/PostImageController.php
+++ fishPost/app/Http/Controllers/PostImageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CookingPost;
 use Illuminate\Http\Request;
 
 class PostImageController extends Controller
@@ -27,14 +28,25 @@ class PostImageController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * 投稿
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        // アップロードされたファイル名を取得
+        $upload_image = $request->file('image_path');
+        // storageへの保存
+        $path = $upload_image->store('uploads', "public");
+
+        CookingPost::create([
+            "product_name" => $request->product_name,
+            "cooking_explanation" => $request->cooking_explanation,
+            "image_path" => $path,
+        ]);
+
+        return redirect()->route('posts.index');
     }
 
     /**
