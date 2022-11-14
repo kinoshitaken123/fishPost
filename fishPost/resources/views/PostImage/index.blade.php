184d59b6e21cfe9a6dec3e26107437fa1f116152
kinoshitaken123 <k1k7s9e3@gmail.com>
2022-11-14 16:59:19 +0900

    feature 詳細リンク追加

--- fishPost/resources/views/PostImage/index.blade.php
+++ fishPost/resources/views/PostImage/index.blade.php
@@ -11,11 +11,12 @@
   @forelse ($post_data as $value)
   <div class="card" style="width: 18rem; display: inline-block;">
     <img src="{{ Storage::url($value->image_path) }}" class="col-auto" style="width:100%;" />
-    <p class="col-auto">{{$value->product_name}}</p>
+    <a href="{{ route('posts.show', $value->id) }}">詳細</a>
+    <p class=" col-auto">{{$value->product_name}}</p>
     <p class="col-auto">{{$value->cooking_explanation}}</p>
   </div>
   @empty
-{{ config('const.index.NO_DISPLAY') }} 
+  {{ config('const.index.NO_DISPLAY') }}
   @endforelse
 
   @endsection
