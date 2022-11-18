69e0a283bbefc66db944942d255c118cf234679d
kinoshitaken123 <k1k7s9e3@gmail.com>
2022-11-18 16:48:02 +0900

    feature edit 投稿フォーム 作成

--- fishPost/resources/views/parts/edit_post.blade.php
+++ fishPost/resources/views/parts/edit_post.blade.php
@@ -1,16 +1,21 @@
-<form method="post" action="{{ route('posts.update') }}" enctype="multipart/form-data">
+<form method="put" action="{{ route('posts.update', $post_data) }}" enctype="multipart/form-data">
   @csrf
   <div class="col-md-7">
     <label class="form-label">料理名</label>
-    <input type="text" name="product_name" class="form-control">
+    <input type="text" value="{{ $post_data->product_name }}" name="product_name" class="form-control">
   </div>
   <div class="col-md-7">
     <label class="form-label">説明</label>
-    <input type="text" name="cooking_explanation" class="form-control">
+    <input type="text" value="{{ $post_data->cooking_explanation }}" name="cooking_explanation" class="form-control">
   </div>
 
   <div class="input-group mb-3">
-    <input type="file" class="form-control" name="image_path" aria-label="Upload">
-    <button type="submit" class="btn btn-primary btn-sm">投稿</button>
+    <img src="{{ Storage::url($post_data->image_path) }}" style="width:20%;" />
+    <!-- <input type="file" class="form-control" name="image_path" value="{{ Storage::url($post_data->image_path) }}" aria-label="Upload"> -->
   </div>
+  <div class="input-group mb-3">
+    <input type="file" class="form-control" name="image_path" aria-label="Upload">
+  </div>
+  <a href="{{ route('posts.index') }}" class="btn btn-primary">一覧に戻る</a>
+  <a href="{{ route('posts.update', $post_data) }}" class="btn btn-success">更新</a>
 </form>
